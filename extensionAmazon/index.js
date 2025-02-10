daaeea721779e15a478e5e90b29a4845ea890427
Jowi <157366219+JowiMesas@users.noreply.github.com>
2025-02-10 19:24:29 +0100

    information images finished

--- extensionAmazon/index.js
+++ extensionAmazon/index.js
@@ -16,10 +16,37 @@ function clickButtonInfoImages() {
   });
 }
 function showInfoImages() {
-    let images = document.querySelectorAll("img");
-    images.forEach((img)=> {
-        if(!img.dataset.tooltipAdded) {
-            
-        }
-    })
+  let images = document.querySelectorAll("img");
+  images.forEach((img) => {
+    if (!img.dataset.tooltipAdded) {
+      img.addEventListener("mouseenter", () => {
+        const informationDiv = document.createElement("div");
+        informationDiv.className = "remove";
+        informationDiv.textContent = img.alt || "No description";
+        informationDiv.style.position = "absolute";
+        informationDiv.style.background = "lightblue";
+        informationDiv.style.padding = "6px";
+        informationDiv.style.border = "1px solid black";
+        informationDiv.style.fontSize = "16px";
+        informationDiv.style.zIndex = 10000;
+        document.body.appendChild(informationDiv);
+        const rect = img.getBoundingClientRect();
+        informationDiv.style.top = `${rect.top + window.scrollY - 30}px`;
+        informationDiv.style.left = `${rect.left}px`;
+      });
+      img.addEventListener("mouseleave", () => {
+        let deleteInformationDiv = document.querySelectorAll(".remove");
+        deleteInformationDiv.forEach((divRemove) => {
+          divRemove.remove();
+        });
+      });
+      img.dataset.tooltipAdded = "true";
+    }
+  });
+}
+let min_price = document.getElementById("btn-min-price");
+min_price.addEventListener("click", clickButtonMinPrice);
+
+function clickButtonMinPrice() {
+  
 }
